33062c3c3c23f960bdd7a49fcae82d028f6b1c08
Dulana Karunathilake <dulanakalya@gmail.com>
2025-07-09 07:56:32 +0530

    navigate removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { ProtectedRoute, AdminRoute } from './service/Guard';
 import Navbar from './component/common/Navbar';
 import { CartProvider } from './component/context/CartContext';
